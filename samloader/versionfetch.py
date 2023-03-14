d319c503a78c1d06ce6d5bda0752d4ddd65a1d77
lukestarlord <42268615+lukestarlord@users.noreply.github.com>
2023-03-14 10:56:48 +0800

    Fix checkupdate command
    Requests for checkupdate command fails without a valid User-Agent, fixed by added curl UA.

--- samloader/versionfetch.py
+++ samloader/versionfetch.py
@@ -17,8 +17,9 @@ def normalizevercode(vercode: str) -> str:
 
 def getlatestver(model: str, region: str) -> str:
     """ Get the latest firmware version code for a model and region. """
+    request_headers = {'User-Agent': 'curl/7.81.0'}
     req = requests.get("https://fota-cloud-dn.ospserver.net/firmware/" \
-        + region + "/" + model + "/version.xml")
+        + region + "/" + model + "/version.xml", headers=request_headers)
     if req.status_code == 403:
         raise Exception("Model or region not found (403)")
     req.raise_for_status()
